bcf7b701f1071de3c06f917675bb2a851be45644
Ben Noble <bennoble@Benjamins-MacBook-Pro.local>
2022-11-30 11:55:27 +0000

    Added blank project

---
+++ AdventOfCode.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace AdventOfCode.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
