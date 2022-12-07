61ca5b4b80118e081759a65b75ea81e6e2c85364
Ben Noble <ben@benshouse.net>
2022-12-07 17:48:37 +0000

    Prep for tomorrow

---
+++ AdventOfCode.Test/Day08/Task02.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace AdventOfCode.Test.Day08;
+
+public class Task02
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test_ExampleData()
+    {
+        var input = new FileInput("Day08/ExampleInput.txt").ReadLines();
+    }
+
+    [Test]
+    public void Test_MyData()
+    {
+        var input = new FileInput("Day08/MyInput.txt").ReadLines();
+    }
+}
